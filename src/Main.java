83161753787894c7310c3c88969b1e7a22df0432
Jamyl Tukhvatullin <sinkler86@gmail.com>
2022-04-01 20:24:10 +0700

    added files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2750;
+        int miles = 20;
+        int bonus = price / miles;
+        System.out.println("Получено миль: " + bonus);
+    }
+}
